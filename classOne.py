69fa52d7ad49f63905f66a163ac3f694e76156d7
Nicolas Moura Costa <101121845+NicolasMouraCosta@users.noreply.github.com>
2024-11-11 23:43:56 +0000

    my firts commit

---
+++ classOne.py
@@ -0,0 +1,9 @@
+nome = input('informe seu nome completo\n')
+idade = input('Informe sua idade\n')
+peso = input('Informe seu peso\n')
+cpf = input('Informe seu cpf\n')
+
+print('\n[','NOME:',nome, ']\n')
+print('[','IDADE:',idade, ']\n')
+print('[','PESO:',peso, ']\n')
+print('[','CPF:',cpf, ']\n')
